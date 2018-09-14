5bbec424c39b5feece1ba87841811c410a49307d
Peter Balogh <p.balogh.sa@gmail.com>
2018-09-14 13:10:15 +0200

    second

--- cmd/main.go
+++ cmd/main.go
@@ -3,27 +3,27 @@ package main
 import (
 	"fmt"
 
-	// "sync"
-	// "encoding/json"
+	"sync"
+	"encoding/json"
 
-	// "github.com/golang/glog"
-	// "github.com/openshift/generic-admission-server/pkg/cmd"
-	// admissionv1beta1 "k8s.io/api/admission/v1beta1"
-	// "k8s.io/api/core/v1"
-	// metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	// "k8s.io/apimachinery/pkg/runtime/schema"
-	// "k8s.io/client-go/dynamic"
-	//  "k8s.io/client-go/rest"
+	"github.com/golang/glog"
+	"github.com/openshift/generic-admission-server/pkg/cmd"
+	admissionv1beta1 "k8s.io/api/admission/v1beta1"
+	"k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
+	 "k8s.io/client-go/rest"
 
-	// "github.com/pbalogh-sa/crd-test/pkg/anchore"
+	"github.com/pbalogh-sa/crd-test/pkg/anchore"
 	"github.com/pbalogh-sa/crd-test/pkg/whitelist"
 )
 
-// type admissionHook struct {
-// 	reservationClient dynamic.ResourceInterface
-// 	lock              sync.RWMutex
-// 	initialized       bool
-// }
+type admissionHook struct {
+	reservationClient dynamic.ResourceInterface
+	lock              sync.RWMutex
+	initialized       bool
+}
 
 func main() {
 	result, err := whitelist.GetWhiteList()
@@ -34,47 +34,47 @@ func main() {
 		fmt.Println(res)
 	}
 
-	// cmd.RunAdmissionServer(&admissionHook{})
+	cmd.RunAdmissionServer(&admissionHook{})
 }
 
-// func (a *admissionHook) ValidatingResource() (plural schema.GroupVersionResource, singular string) {
-// 	return schema.GroupVersionResource{
-// 			Group:    "poke.admission.anchore.io",
-// 			Version:  "v1beta1",
-// 			Resource: "imagechecks",
-// 		},
-// 		"imagecheck"
-// }
+func (a *admissionHook) ValidatingResource() (plural schema.GroupVersionResource, singular string) {
+	return schema.GroupVersionResource{
+			Group:    "poke.admission.anchore.io",
+			Version:  "v1beta1",
+			Resource: "imagechecks",
+		},
+		"imagecheck"
+}
 
-// func (a *admissionHook) Validate(admissionSpec *admissionv1beta1.AdmissionRequest) *admissionv1beta1.AdmissionResponse {
-// 	status := &admissionv1beta1.AdmissionResponse{
-// 		Allowed: true,
-// 		UID:     admissionSpec.UID,
-// 		Result:  &metav1.Status{Status: "Success", Message: ""}}
+func (a *admissionHook) Validate(admissionSpec *admissionv1beta1.AdmissionRequest) *admissionv1beta1.AdmissionResponse {
+	status := &admissionv1beta1.AdmissionResponse{
+		Allowed: true,
+		UID:     admissionSpec.UID,
+		Result:  &metav1.Status{Status: "Success", Message: ""}}
 
-// 	if admissionSpec.Kind.Kind == "Pod" {
-// 		pod := v1.Pod{}
-// 		json.Unmarshal(admissionSpec.Object.Raw, &pod)
-// 		for _, container := range pod.Spec.Containers {
-// 			image := container.Image
-// 			glog.Info("Checking image: " + image)
-// 			if !anchore.CheckImage(image) {
-// 				status.Result.Status = "Failure"
-// 				status.Allowed = false
-// 				message := fmt.Sprintf("Image failed policy check: %s", image)
-// 				status.Result.Message = message
-// 				glog.Warning(message)
-// 				return status
-// 			} else {
-// 				glog.Info("Image passed policy check: " + image)
-// 				glog.Info("aaaaaaaa" + pod.Name
-// 			}
-// 		}
+	if admissionSpec.Kind.Kind == "Pod" {
+		pod := v1.Pod{}
+		json.Unmarshal(admissionSpec.Object.Raw, &pod)
+		for _, container := range pod.Spec.Containers {
+			image := container.Image
+			glog.Info("Checking image: " + image)
+			if !anchore.CheckImage(image) {
+				status.Result.Status = "Failure"
+				status.Allowed = false
+				message := fmt.Sprintf("Image failed policy check: %s", image)
+				status.Result.Message = message
+				glog.Warning(message)
+				return status
+			} else {
+				glog.Info("Image passed policy check: " + image)
+				glog.Info("aaaaaaaa" + pod.Name
+			}
+		}
 
-// 	}
-// 	return status
-// }
+	}
+	return status
+}
 
-// func (a *admissionHook) Initialize(kubeClientConfig *rest.Config, stopCh <-chan struct{}) error {
-// 	return nil
-// }
+func (a *admissionHook) Initialize(kubeClientConfig *rest.Config, stopCh <-chan struct{}) error {
+	return nil
+}
