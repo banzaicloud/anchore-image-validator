de826fee4542861cedc4dc22602d2df67a8fb672
Peter Balogh <p.balogh.sa@gmail.com>
2021-02-25 09:28:26 +0100

    build: update golang version
    Signed-off-by: Peter Balogh <p.balogh.sa@gmail.com>

--- pkg/anchore/client.go
+++ pkg/anchore/client.go
@@ -100,8 +100,6 @@ func getStatus(digest string, tag string) bool {
 
 	path := fmt.Sprintf("/v1/images/%s/check?%s", digest, params.Encode())
 
-	// path := fmt.Sprintf("/v1/images/%s/check?history=false&detail=false&tag=%s", digest, tag)
-
 	body, err := anchoreRequest(path, nil, "GET")
 
 	if err != nil {
